84c4d619b644200ece4787a5da3cb30be4911df0
aldxc17 <aldcpp9@gmail.com>
2026-05-28 09:22:42 -0600

    Se agregó la practica de suma de 3 números

---
+++ AldoCésarPérezPérez.py
@@ -0,0 +1,6 @@
+num1 = float(input("Ingresa el primero número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+resultado =  num1 + num2 + num3
+print(f"\nLa suma de {num1} + {num2} + {num3} es: {resultado}")
